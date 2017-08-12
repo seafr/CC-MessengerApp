2cfd4395d8035e5189a31a56fec3adf26e0c9402
Sea <seafr@users.noreply.github.com>
2017-08-12 23:32:05 +0400

    Edit db/seeds.rb to create sample messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+5.times do |i|
+  Message.create(content: "Message #{i+1}")
+end
