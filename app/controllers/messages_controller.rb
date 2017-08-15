f5b11b7bd782878af2fec2cf55ecb6e71c3b1e24
Sea <seafr@users.noreply.github.com>
2017-08-15 22:53:51 +0400

    Create route 'messages' (POST) and action 'create' (in controller) for saving new message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,4 +6,20 @@ class MessagesController < ApplicationController
   def new
     @message = Message.new
   end
+
+  def create
+    @message = Message.new(message_params)
+
+    if @message.save
+      redirect_to '/messages'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def message_params
+      params.require(:message).permit(:content)
+    end
 end
