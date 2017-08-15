e25a62f1942c358d93390697cf2f1804100c8e54
Sea <seafr@users.noreply.github.com>
2017-08-15 22:43:48 +0400

    Create route 'messages/new' and action 'new' (in controller) for new message

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get '/messages' => 'messages#index'
+
+  get 'messages/new' => 'messages#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
